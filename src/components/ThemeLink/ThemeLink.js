738d7c5db254b99bf21005cc8383b98f91247d4d
Dom <d_lucenario@yahoo.com.ph>
2022-03-04 21:19:38 +0800

    about page responsiveness

--- src/components/ThemeLink/ThemeLink.js
+++ src/components/ThemeLink/ThemeLink.js
@@ -6,8 +6,8 @@ import * as styles from './ThemeLink.module.css';
 const ThemeLink = ({children, to, onClick, isActive, themeRef}) => {
 
   return (
-    <div className={`${styles.root} ${isActive === true ? styles.active: ''}`} ref={themeRef}>
-      <Link onClick={onClick} className={`${styles.link}`} to={to}>{children}</Link>
+    <div  onClick={onClick} role={'presentation'} className={`${styles.root} ${isActive === true ? styles.active: ''}`} ref={themeRef}>
+      <Link className={`${styles.link}`} to={to}>{children}</Link>
     </div>
   );
 };
