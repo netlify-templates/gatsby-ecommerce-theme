5a9e1a6a41193b8c7ffbc209db1fd316049508e4
Ash Durham <ash@matterdesign.com.au>
2022-05-12 09:38:57 +1000

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
 module.exports = {
     siteMetadata: {
-      title: `My Gatsby Site`,
-        siteUrl: `https://www.yourdomain.tld`,
+      title: `Gatsby Sydney Ecommerce Theme`,
+        siteUrl: `https://jamm.matter.design`,
     },
     plugins: [
-
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `Gatsby Sydney Ecommerce Theme`,
+          short_name: `Sydney`,
+          start_url: `/`,
+          background_color: `#000000`,
+          theme_color: `#ffffff`,
+          display: `standalone`,
+          icon: 'src/assets/favicon.png'
+        },
+      },
     ]
 }
