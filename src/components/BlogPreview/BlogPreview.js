6823f95908292b100f0ce5a9ca31aeedc3c74d39
Prince Wilson <maxcell.wilson@gmail.com>
2022-05-18 13:57:54 -0400

    chore: Make blog list use anchors for titles and increase touch target

--- src/components/BlogPreview/BlogPreview.js
+++ src/components/BlogPreview/BlogPreview.js
@@ -3,26 +3,27 @@ import { Link, navigate } from 'gatsby';
 import * as styles from './BlogPreview.module.css';
 
 const BlogPreview = (props) => {
-  const {image, altImage, title, link, category, hideReadMoreOnWeb, showExcerpt, excerpt} = props;
+  const {image, altImage, title, link, category, showExcerpt, excerpt} = props;
+
+  const handleClick = () => {
+    navigate('/blog/sample');
+  };
+
   return (
-    <div className={styles.root}>
+    <div className={styles.root} onClick={handleClick}>
         <img 
-          onClick={() => navigate(link)} 
           className={styles.blogPreviewImage} 
           alt={altImage} 
           src={image}
           role={'presentation'} 
         />
         <span className={styles.category}>{category}</span>
-        <h4 className={styles.title}>{title}</h4>
+        <h4 className={styles.title}><Link to={link}>{title}</Link></h4>
         {showExcerpt && 
           <p className={styles.excerpt}>
             {excerpt}
           </p>
         }
-        <Link className={`${styles.link} ${hideReadMoreOnWeb === true ? styles.hideReadMoreOnWeb : ''}`} to={link}>
-          read more
-        </Link>
     </div>
   );
 };
