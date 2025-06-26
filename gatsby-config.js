977a5a21e4ff01e00f0c150863f646826aadc910
akivatec <akivasoporte@gmail.com>
2025-06-26 16:32:22 -0500

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,21 @@
+// Archivo: gatsby-config.js
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Sydney Ecommerce Theme`,
-    siteUrl: `https://jamm.matter.design`,
+    title: `Gatsby Ecommerce Theme`,
+    siteUrl: `https://akiva.com.co`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 };
