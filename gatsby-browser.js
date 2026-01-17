c5f04511a0ff597aee7c9472f71d8598bf76a3a9
habibsh786 <Habibshah889922@gmail.com>
2026-01-17 15:18:38 +0500

    Add onRenderBody to include micro-survey widget
    widget

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,14 @@ import { NotificationProvider } from './src/context/AddItemNotificationProvider'
 export const wrapRootElement = ({ element }) => (
   <NotificationProvider>{element}</NotificationProvider>
 );
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      key="micro-survey-widget"
+      src="<WIDGET_URL>"
+      data-sitekey="<WEBSITE_KEY>"
+      async
+    />,
+  ]);
+};
